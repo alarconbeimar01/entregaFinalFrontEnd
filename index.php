631a3ada7058a86d693854418420f8c63e6a98a5
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-21 18:11:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,35 +6,39 @@ header("location: entrada.php");
 }
 ?>
 <!DOCTYPE HTML>
-<html>
+<html lang="es">
 <head>
-
-<title>YP</title>
-<!-- Custom Theme files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<!-- for-mobile-apps -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<meta name="keywords" content="Flat Login Form Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<!-- //for-mobile-apps -->
-<!--Google Fonts-->
-<link href='//fonts.googleapis.com/css?family=Signika:400,600' rel='stylesheet' type='text/css'>
-<!--google fonts-->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+  <!-- Styles -->
+  <link rel="stylesheet" href="./css/styles-login.css">
+  <!-- Title -->
+  <title>Iniciar Sesión</title>
 </head>
 <body>
-<br>
-<!--header start here-->
-
-<div class="header agile">
-	<div class="wrap">
-		<div class="login-main wthree">
-			<div class="login">
-			<h3>Ingresar</h3>
-			<form action="index.php" method="post">
-				<input type="text" placeholder="Usuario" required name="username" required >
-				<input type="password" placeholder="Contraseña" name="password" required>
-				<input name="submit" type="submit" value="Ingresar">
+<header class="header"></header>
+  <section class="login">
+      <section class="login__container">
+        <h2>Inicia sesión</h2>
+			<form class="login__container--form" action="index.php" method="post">
+				<input class="input" type="text" placeholder="Usuario" required name="username" required >
+				<input class="input" type="password" placeholder="Contraseña" name="password" required>
+				<input class="button" name="submit" type="submit" value="Ingresar">
+				
+				<div class="login__container--remember-me">
+			  
+			
+			  
+            <label>
+              <input type="checkbox" name="" id="cbox1" value="checkbos">Recuérdame
+            </label>
+            <a href="/">Olvidé mi contraseña</a>
+          </div>
+				
+				
 			</form>
 			<div class="clear"> </div>
 				<p><span><?php echo $error; ?></span>
