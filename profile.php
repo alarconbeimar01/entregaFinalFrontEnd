e69ecfbbd8713c2b78c230356854bd7c1db6f515
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-23 09:48:21 -0500

    cabecera del  atiguo php

---
+++ profile.php
@@ -0,0 +1,37 @@
+<?php
+include('session.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<title>YP</title>
+<!-- Custom Theme files -->
+
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Flat Login Form Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+			    
+                <link href="css/menu.css" rel="stylesheet" type="text/css">
+                <link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css">
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<div align="right">
+                  
+                  <table  border="0">
+                    <tr>
+                      
+                      <td>Usuario :</td>
+                      <td><i><?php echo $login_session; ?></i></td>
+                      <td><a href="logout.php"> &nbsp; Salir</a></td>
+                      <td><img src="igames/iconologo.png" alt="" width="30" height="30"></td>
+                    </tr>
+                  </table>
+</div>
+    
+   
+
+<!--</html> -->
