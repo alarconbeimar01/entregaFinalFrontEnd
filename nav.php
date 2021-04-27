e863e54113b1653c70a109c158cc4d33224aa416
Beimar Alarcon <alarconbeimar@gmail.com>
2021-04-27 08:39:57 -0500

    cambio en el menu

---
+++ nav.php
@@ -0,0 +1,28 @@
+<nav class="nav-collapse">
+
+      <ul>
+         
+          
+        <li><a href="listarClientes.php">Listar Clientes</a></li>
+        
+        <li><a href="nuevoCliente.php">Agregar Cliente</a></li>
+          
+         
+        <li><a href="recaudar.php">Recaudar</a></li>
+        <li><a href="reporteRecaudo.php">Reporte Recaudos</a></li>
+          
+         <li><a href="listarUsuarios.php">Usuarios</a></li>
+        <li><a href="listarPlanes.php">Planes</a></li>
+           
+        <li><a href="porInstalar.php">Instalaciones</a></li>
+        <li><a href="porSuspender.php">Suspenciones</a></li>
+          
+          
+         <li><a href="porReconectar.php">Reconexiones</a></li>
+          
+        <li><a href="generarCobrar.php">Mensualidades</a></li>
+            
+      </ul>
+        
+
+    </nav>
